e533f3fd9a6ec0f79173772e9542f58f805e1b39
luana <irish.coffee.429@hotmail.com>
2021-03-18 15:32:16 +0800

    modify competition entity list
    修改 参赛作品列表展示更改

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,9 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    works: []
+    competitors: [],
+    hrNode: []
   },
-  goToItem(e){
+  goToDetail(e){
     console.log(e)
     wx.navigateTo({
       url: '../item/item?id=' + e.currentTarget.id,
@@ -17,31 +18,36 @@ Page({
       hrNode: [{
         name: 'hr',
         attrs: {
-          style: 'line-height: 2px;'
+          style: 'line-height: 2rpx;'
         }
       }],
-      works: [{
-        competitor: "A",
-        roseEntries: {
+      competitors: [{
+        competitorId: "A",
+        competitor: "person a",
+        roseEntries: [{
           name: "EarthAngle",
           url: "./img/EarthAngle/EarthAngle10.jpg"
-        }
+        }, {
+          name: "Novalis",
+          url: "./img/Novalis/Novalis1.jpg"
+        }]
       }, {
-        url: "./img/Novalis/Novalis1.jpg",
-        roseName: "Novalis",
-        competitor: "B"
+        competitorId: "B",
+        competitor: "person b",
+        roseEntries: [{
+          name: "Florentina",
+          url: "./img/Florentina/Florentina1.jpg"
+        }, {
+          name: "SunnySky",
+          url: "./img/SunnySky/SunnySky1.jpg"
+        }]
       }, {
-        url: "./img/Florentina/Florentina1.jpg",
-        roseName: "Florentina",
-        competitor: "C"
-      }, {
-        url: "./img/SunnySky/SunnySky1.jpg",
-        roseName: "SunnySky",
-        competitor: "D"
-      }, {
-        url: "./img/Rosanna/Rosanna1.jpg",
-        roseName: "Rosanna",
-        competitor: "E"
+        competitorId: "C",
+        competitor: "person c",
+        roseEntries: [{
+          name: "Rosanna",
+          url: "./img/Rosanna/Rosanna1.jpg"
+        }]
       }]
     })
   }
