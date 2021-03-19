23210481c23fd39ad5a80df104fcd093bc161e5a
luana <irish.coffee.429@hotmail.com>
2021-03-19 12:49:56 +0800

    add choose image in join page
    新增 参赛页面增加选择图片并显示功能，图片支持单张放大预览

--- pages/join/join.js
+++ pages/join/join.js
@@ -1,54 +1,35 @@
-// index.js
+// join.js
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    competitors: [],
-    hrNode: []
+    uploadedPhotoPaths: [],
+    uploadedPhotos: []
   },
-  goToDetail(e){
-    console.log(e)
-    wx.navigateTo({
-      url: '../item/item?id=' + e.currentTarget.id,
+
+  chooseImage() {
+    wx.chooseImage({
+      success: (arg) => {
+        var filePaths = this.data.uploadedPhotoPaths.concat(arg.tempFilePaths)
+        var files = this.data.uploadedPhotos.concat(arg.tempFiles)
+        this.data.uploadedPhotoPaths = filePaths
+        this.data.uploadedPhotos = files
+
+        this.setData({
+          uploadedPhotoPaths:  filePaths
+        })
+        console.log(this.data)
+      }
     })
   },
-  onLoad() {
-    this.setData({
-      hrNode: [{
-        name: 'hr',
-        attrs: {
-          style: 'line-height: 2rpx;'
-        }
-      }],
-      competitors: [{
-        competitorId: "A",
-        competitor: "person a",
-        roseEntries: [{
-          name: "EarthAngle",
-          url: "./img/EarthAngle/EarthAngle10.jpg"
-        }, {
-          name: "Novalis",
-          url: "./img/Novalis/Novalis1.jpg"
-        }]
-      }, {
-        competitorId: "B",
-        competitor: "person b",
-        roseEntries: [{
-          name: "Florentina",
-          url: "./img/Florentina/Florentina1.jpg"
-        }, {
-          name: "SunnySky",
-          url: "./img/SunnySky/SunnySky1.jpg"
-        }]
-      }, {
-        competitorId: "C",
-        competitor: "person c",
-        roseEntries: [{
-          name: "Rosanna",
-          url: "./img/Rosanna/Rosanna1.jpg"
-        }]
-      }]
+
+  previewImg: (e) => {
+    console.log(this.data)
+    wx.previewImage({
+      current: this.data.uploadedPhotos[e.currentTarget.dataset.idx],
+      urls: this.data.uploadedPhotos
     })
-  }
+  },
+  onLoad() {}
 })
